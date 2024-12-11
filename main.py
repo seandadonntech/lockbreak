a1a1ab5cad3159129b899be183c5f33d46c797be
lilcryptotech <lilcryptotech@gmail.com>
2024-12-11 17:42:54 -0500

    ignore th git ingorne file

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ from pyfiglet import figlet_format
 
 colorama.init()
 
-def generate_captial_code():
+def generate_capital_code(num_words):
     letters = string.ascii_uppercase
-    code = ''.join(random.choice(letters) for i in range(4))
-   
-    green_code = colorama.Fore.GREEN + code + colorama.Fore.RESET
-    return green_code
+    codes = []
+
+    for _ in range(num_words):
+        code = ''.join(random.choice(letters) for _ in range(4))
+        green_code = colorama.Fore.GREEN + code + colorama.Fore.RESET
+        codes.append(green_code)
+
+    return '\n'.join(codes)
 
 banner = figlet_format("lock break", font="slant")
 print(colorama.Fore.GREEN + banner + colorama.Fore.RESET)
-print("Welcome to lock break a program designed to help you with your lock ")
+print("Welcome to lock break, a program designed to help you with your lock.")
 
-option = input("Enter your option: \n")
-
-if option == "1":
-    print(generate_captial_code())
+try:
+    num_words = int(input("Enter the number of codes you want: "))
+    if num_words <= 0:
+        raise ValueError("The number of codes must be a positive integer.")
+except ValueError as e:
+    print(f"Invalid input: {e}")
+else:
+    print(generate_capital_code(num_words))
 
 colorama.deinit()
